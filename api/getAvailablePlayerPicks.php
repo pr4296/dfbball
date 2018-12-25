dd18a9f82f8bd9afca685bddca17d52fb425c074
Pratyush Rustagi <pr4296@gmail.com>
2018-12-25 16:35:56 -0600

    getAvailablePlayerPicks endpoint

---
+++ api/getAvailablePlayerPicks.php
@@ -0,0 +1,21 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$creds = json_decode(file_get_contents('../auth/mysql_auth.json'), true);
+$host = $creds['host'];
+$user = $creds['user'];
+$passwd = $creds['passwd'];
+$db = $creds['db'];
+
+$mysqli = new mysqli($host, $user, $passwd, $db);
+
+$query = "select * from available_players a inner join player p on a.playerId = p.id inner join player_season_totals pst on a.playerId = pst.playerId;";
+
+$result = $mysqli->query($query);
+
+$rows = [];
+while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+}
+echo json_encode($rows);
+
+?>
