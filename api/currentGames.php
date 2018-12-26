4ea02d5a1edeaf45286cf3fbc8751ca7f7568e36
Pratyush Rustagi <pr4296@gmail.com>
2018-12-25 23:53:35 -0600

    debugging currentgames

--- api/currentGames.php
+++ api/currentGames.php
@@ -28,7 +28,7 @@ if (!is_numeric($h)) {
 }
 $h = ($h*24)+6;
 
-$query = "SELECT h.teamName as homeTeamName, h.city as homeCity, a.teamName as awayTeamName, a.city as awayCity, g.* FROM game g inner join team h on h.id = g.homeTeamId inner join team a on a.id = g.awayTeamId WHERE DATE(DATE_SUB(startTime, INTERVAL 6 HOUR) = DATE(DATE_SUB(now(), INTERVAL ".$h." HOUR))  ".$and." ORDER BY startTime asc, awayTeamId asc";
+$query = "SELECT h.teamName as homeTeamName, h.city as homeCity, a.teamName as awayTeamName, a.city as awayCity, g.* FROM game g inner join team h on h.id = g.homeTeamId inner join team a on a.id = g.awayTeamId WHERE DATE(DATE_SUB(startTime, INTERVAL 6 HOUR)) = DATE(DATE_SUB(now(), INTERVAL ".$h." HOUR)) ".$and." ORDER BY startTime asc, awayTeamId asc";
 // echo $query."\n";
 $result = $mysqli->query($query);
 $rows = [];
