3aa358f0f168b5b4229ec83095905e93662dab60
Pratyush Rustagi <pr4296@gmail.com>
2018-12-27 23:55:48 +0000

    fixed getUserPicks endpoint

--- api/getUserPicks.php
+++ api/getUserPicks.php
@@ -24,6 +24,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     $rows[] = $row;
 }
 
+
 if (sizeof($rows) != 5) {
     $d['message'] = "Picks were not found.";
     echo json_encode($d);
@@ -33,12 +34,21 @@ if (sizeof($rows) != 5) {
 $res = [];
 for ($i = 0; $i < 5; $i++) {
     $query = "select *, (d.pts+d.fg3PtMade*0.5+(d.offReb+d.defReb)*1.25+d.ast*1.5+d.stl*2+d.blk*2+d.tov*-0.5) as fpts, (d.offReb+d.defReb) as reb from daily_player_box_stats d inner join player p on d.playerId = p.id where d.playerId =".$rows[$i]['playerId']." and DATE(DATE_SUB(d.startTime, INTERVAL 6 HOUR)) = DATE(DATE_SUB(NOW(), INTERVAL 6 HOUR))";
-    
-    if (!result) {
-        $query = "select * from player p inner join player_season_totals s on p.id = s.playerId where s.playerId =".$rows[$i]['playerId'];
+    $result = $mysqli->query($query);
+
+    $t = mysqli_fetch_assoc($result);
+
+    if (!$t) {
+        $query = "select * from player p inner join player_season_totals s on p.id = s.playerId where s.playerId =".$rows[$i]["playerId"];
     }
 
     $result = $mysqli->query($query);
+    if (!$result) {
+	$d['message'] = "Something went wrong with the query.";
+        echo json_encode($d);
+        exit(1);
+    }
+
     $row = mysqli_fetch_assoc($result);
     $res[] = $row;
 }
