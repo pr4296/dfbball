4819fd38b8378dd3520ed80806e899953d5a0fd4
Pratyush Rustagi <pr4296@gmail.com>
2019-01-05 17:41:34 -0700

    filtered out players who aren't playing

--- api/getGameInfo.php
+++ api/getGameInfo.php
@@ -25,7 +25,7 @@ $rows = [];
 $row = mysqli_fetch_assoc($result);
 $res["gameInfo"] = $row;
 
-$query = "select *, (pts+fg3PtMade*0.5+(offReb+defReb)*1.25+ast*1.5+stl*2+blk*2+tov*-0.5) as fpts from daily_player_box_stats d inner join player p on p.id = d.playerId inner join team t on t.id = d.playerTeamId where gameId = ".$id." order by fpts desc;";
+$query = "select *, (pts+fg3PtMade*0.5+(offReb+defReb)*1.25+ast*1.5+stl*2+blk*2+tov*-0.5) as fpts from daily_player_box_stats d inner join player p on p.id = d.playerId inner join team t on t.id = d.playerTeamId where gameId = ".$id." and d.minSeconds > 0 order by fpts desc;";
 
 $result = $mysqli->query($query);
 
